9085dd8c721b52be5a6251ce5a6f915d307130fa
Ubuntu <ubuntu@ip-172-31-18-25.ec2.internal>
2026-09-22 01:24:16 +0000

    Configura proxy inverso para rate limiting

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 // ============================================================================
